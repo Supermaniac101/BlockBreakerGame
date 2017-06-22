01810d67384316ed545833209e7ef4251aca88dd
Supermaniac101 <Timschijvenaars@hotmail.com>
2017-06-22 20:57:02 +0200

    Initial commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+
+    public void LoadLevel(string levelname)
+    {
+        SceneManager.LoadScene(levelname);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public static MusicPlayer mp = null;
+
+    void Awake()
+    {
+        if (mp != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            mp = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    
+	}
+
+
+}
